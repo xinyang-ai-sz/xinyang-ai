31b8d4b4834301238a54caa082d6fdf47f388451
ding.wan <ding.wan@xinyang-ai.com>
2025-03-24 22:04:28 +0800

    remove dist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   base: '/',
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: './',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
