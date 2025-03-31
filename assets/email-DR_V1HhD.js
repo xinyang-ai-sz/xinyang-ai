4888f2284424481fb9aefe7491d61dd66528d399
ding.wan <ding.wan@xinyang-ai.com>
2025-03-31 19:22:31 +0800

    Updates

--- assets/email-DR_V1HhD.js
+++ assets/email-DR_V1HhD.js
@@ -1,4 +1,4 @@
-import{s as a}from"./index-BS1JLh1n.js";const m=`
+import{s as a}from"./index-Cifej_b_.js";const m=`
 <div style="font-family: sans-serif;">
   <h2>新留言通知</h2>
   <p>留言人: {{name}}</p>
