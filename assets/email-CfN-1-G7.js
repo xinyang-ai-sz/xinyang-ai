e530f5f2fd468b8f705b0550de3ff3e9515d979f
ding.wan <ding.wan@xinyang-ai.com>
2025-04-02 12:31:50 +0800

    Updates

--- assets/email-CfN-1-G7.js
+++ assets/email-CfN-1-G7.js
@@ -1,4 +1,4 @@
-import{s as a}from"./index-BnqdzTDw.js";const m=`
+import{s as a}from"./index-CD__u3pK.js";const m=`
 <div style="font-family: sans-serif;">
   <h2>新留言通知</h2>
   <p>留言人: {{name}}</p>
