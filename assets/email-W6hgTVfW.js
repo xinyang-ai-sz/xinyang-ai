41f72add34dce2fa55ff20c2cdeb73c38cc5008f
ding.wan <ding.wan@xinyang-ai.com>
2025-04-02 15:45:15 +0800

    Updates

--- assets/email-W6hgTVfW.js
+++ assets/email-W6hgTVfW.js
@@ -1,4 +1,4 @@
-import{s as a}from"./index-BDOuLa4o.js";const m=`
+import{s as a}from"./index-BhTB3gpo.js";const m=`
 <div style="font-family: sans-serif;">
   <h2>新留言通知</h2>
   <p>留言人: {{name}}</p>
