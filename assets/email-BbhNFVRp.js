4626388c84540295177f369aade551b46a544fa0
ding.wan <ding.wan@xinyang-ai.com>
2025-04-07 15:27:24 +0800

    Updates

--- assets/email-BbhNFVRp.js
+++ assets/email-BbhNFVRp.js
@@ -1,4 +1,4 @@
-import{s as a}from"./index-BE4Ktotb.js";const m=`
+import{s as a}from"./index-BeRltm8U.js";const m=`
 <div style="font-family: sans-serif;">
   <h2>新留言通知</h2>
   <p>留言人: {{name}}</p>
